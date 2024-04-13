c12e1a57116af1169069853fdd8ff77c1d900e77
Dilson <dilsono316@gmail.com>
2024-04-13 09:27:31 -0300

    message component

---
+++ src/app/components/message-section/message-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-section.component.html',
+  styleUrl: './message-section.component.scss'
+})
+export class MessageSectionComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { MainSectionComponent } from '../../components/main-section/main-section.component';
+import { MessageSectionComponent } from '../../components/message-section/message-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,MainSectionComponent],
+  imports: [NavbarComponent,MainSectionComponent,MessageSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
