8e674f0546f63b3be9b1fbfeac71f299125ef656
Dilson <dilsono316@gmail.com>
2024-04-13 09:44:22 -0300

    router pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateCompanyComponent } from './pages/companies/create-company/create-company.component';
+import { CreatePartnerComponent } from './pages/partners/create-partner/create-partner.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',component:LoginComponent},
+    {path:'',component:HomeComponent},
+    {path:'criar/empresa',component:CreateCompanyComponent},
+    {path:'criar/socio',component:CreatePartnerComponent}
+];
---
+++ src/app/pages/partners/create-partner/create-partner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-partner',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-partner.component.html',
+  styleUrl: './create-partner.component.scss'
+})
+export class CreatePartnerComponent {
+
+}
