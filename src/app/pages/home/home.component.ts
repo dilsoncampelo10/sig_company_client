3d72b7b639e72caac57c92f84dbff0a919db1708
Dilson <dilsono316@gmail.com>
2024-04-13 09:19:42 -0300

    section main home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { MainSectionComponent } from '../../components/main-section/main-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,MainSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
